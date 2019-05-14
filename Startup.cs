28b07c3103050057af6e89474e422e7bbfb97ca0
Ishari <mastoffp@gmail.com>
2019-05-14 23:16:13 +0200

    Change references to IdentityUser to ApplicationUser

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using CawCaw.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using CawCaw.Models;
 
 namespace CawCaw
 {
@@ -38,7 +39,7 @@ namespace CawCaw
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
+            services.AddDefaultIdentity<ApplicationUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 

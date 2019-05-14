db5b0b0f5cddc4b7e75697065029a11c88270216
Ishari <mastoffp@gmail.com>
2019-05-14 23:27:43 +0200

    Scaffold Login, Register, and Logout pages

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using CawCaw.Data;
+using CawCaw.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CawCaw.Areas.Identity.IdentityHostingStartup))]
+namespace CawCaw.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
